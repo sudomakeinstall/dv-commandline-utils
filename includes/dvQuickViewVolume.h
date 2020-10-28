e22ad5c0522ed840637a8ee708f3423ff836a156
Davis Vigneault <davis.vigneault@gmail.com>
2020-10-28 14:50:22 -0700

    ENH: Pad volume before visualizing

--- includes/dvQuickViewVolume.h
+++ includes/dvQuickViewVolume.h
@@ -2,6 +2,7 @@
 #define dv_QuickViewVolume_h
 
 #include <itkQuadEdgeMesh.h>
+#include <itkConstantPadImageFilter.h>
 #include <itkCuberilleImageToMeshFilter.h>
 #include <dvITKTriangleMeshToVTKPolyData.h>
 #include <dvQuickViewPolyData.h>
@@ -12,11 +13,21 @@ template<typename TImage>
 void
 QuickViewVolume(typename TImage::Pointer volume) {
 
+  using TPad = itk::ConstantPadImageFilter<TImage, TImage>;
   using TMesh = itk::QuadEdgeMesh< double, 3 >;
   using TCuberille = itk::CuberilleImageToMeshFilter<TImage, TMesh>;
 
+  typename TImage::SizeType padding;
+  padding.Fill(1);
+
+  const auto pad = TPad::New();
+  pad->SetInput(volume);
+  pad->SetPadUpperBound(padding);
+  pad->SetPadLowerBound(padding);
+  pad->SetConstant(static_cast<typename TImage::PixelType>(0));
+
   const auto cuberille = TCuberille::New();
-  cuberille->SetInput( volume );
+  cuberille->SetInput( pad->GetOutput() );
   cuberille->ProjectVerticesToIsoSurfaceOff();
   cuberille->SavePixelAsCellDataOn();
   cuberille->Update();

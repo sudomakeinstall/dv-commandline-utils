98ce79ed5329e8d2955a77cd969744cc2525857a
Davis Vigneault <davis.vigneault@gmail.com>
2021-01-13 19:32:11 -0800

    BUG: Use the correct number of colors in LUT

--- src/dvGetLookupTable.cxx
+++ src/dvGetLookupTable.cxx
@@ -18,21 +18,21 @@ Rainbow()
 
   // Create a lookup table to map cell data to colors
   const auto lut = vtkSmartPointer<vtkLookupTable>::New();
-  lut->SetNumberOfTableValues(9);
+  lut->SetNumberOfTableValues(10);
   lut->Build();
 
-  // Fill in a few known colors, the rest will be generated if needed
   vtkSmartPointer<vtkNamedColors> colors =
     vtkSmartPointer<vtkNamedColors>::New();
-  lut->SetTableValue(0, colors->GetColor4d("Black").GetData());
-  lut->SetTableValue(1, colors->GetColor4d("Crimson").GetData());
-  lut->SetTableValue(2, colors->GetColor4d("Orange").GetData());
-  lut->SetTableValue(3, colors->GetColor4d("Yellow").GetData());
-  lut->SetTableValue(4, colors->GetColor4d("Green").GetData());
-  lut->SetTableValue(5, colors->GetColor4d("Blue").GetData());
-  lut->SetTableValue(6, colors->GetColor4d("Purple").GetData());
-  lut->SetTableValue(7, colors->GetColor4d("Cyan").GetData());
-  lut->SetTableValue(8, colors->GetColor4d("Magenta").GetData());
+  lut->SetTableValue(0, 0.0, 0.0, 0.0, 1.0);
+  lut->SetTableValue(1, 1.0, 0.0, 0.0, 1.0);
+  lut->SetTableValue(2, 1.0, 1.0, 0.0, 1.0);
+  lut->SetTableValue(3, 0.5, 1.0, 0.0, 1.0);
+  lut->SetTableValue(4, 1.0, 0.0, 1.0, 1.0);
+  lut->SetTableValue(5, 1.0, 0.5, 0.0, 1.0);
+  lut->SetTableValue(6, 0.0, 0.5, 1.0, 1.0);
+  lut->SetTableValue(7, 1.0, 0.0, 0.5, 1.0);
+  lut->SetTableValue(8, 0.0, 0.25, 0.5, 1.0);
+  lut->SetTableValue(9, 0.5, 0.0, 0.25, 1.0);
 
   const double gray = 0.3;
   lut->SetBelowRangeColor( gray, gray, gray, 1.0 ); // Gray

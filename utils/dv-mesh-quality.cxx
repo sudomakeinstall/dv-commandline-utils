6ca444069d201dbc5a09c62db474962458727c3f
Davis Vigneault <davis.vigneault@gmail.com>
2021-02-05 19:48:38 -0800

    ENH: Update Mesh Quality

--- utils/dv-mesh-quality.cxx
+++ utils/dv-mesh-quality.cxx
@@ -9,12 +9,13 @@ namespace po = boost::program_options;
 #include <vtkDoubleArray.h>
 #include <vtkFieldData.h>
 #include <vtkMeshQuality.h>
-#include <vtkOBJReader.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataReader.h>
 #include <vtkSmartPointer.h>
 
 using TCoordinate = float;
 
-using TReader = vtkSmartPointer<vtkOBJReader>;
+using TReader = vtkSmartPointer<vtkPolyDataReader>;
 using TQuality = vtkSmartPointer<vtkMeshQuality>;
 
 int
@@ -31,8 +32,7 @@ main(int argc, char** argv)
   quality_map[VTK_QUALITY_MAX_ANGLE] = "TriangleMaxAngle";
   quality_map[VTK_QUALITY_CONDITION] = "TriangleCondition";
   quality_map[VTK_QUALITY_SCALED_JACOBIAN] = "TriangleScaledJacobian";
-  quality_map[VTK_QUALITY_RELATIVE_SIZE_SQUARED] =
-    "TriangleRelativeSizeSquared";
+  quality_map[VTK_QUALITY_RELATIVE_SIZE_SQUARED] = "TriangleRelativeSizeSquared";
   quality_map[VTK_QUALITY_SHAPE] = "TriangleShape";
   quality_map[VTK_QUALITY_SHAPE_AND_SIZE] = "TriangleShapeAndSize";
   quality_map[VTK_QUALITY_DISTORTION] = "TriangleDistortion";
@@ -46,15 +46,14 @@ main(int argc, char** argv)
 
   // Declare the supported options.
   po::options_description description("Allowed options");
-  description.add_options()("help", "Print usage information.")(
-    "input-mesh",
-    po::value<std::string>()->required(),
-    "Filename of the input mesh.")(
-    "metric",
-    po::value<int>()->default_value(VTK_QUALITY_RADIUS_RATIO),
-    metric_description.c_str())("cellwise",
-                                po::value<bool>()->default_value(false),
-                                "Calculate cellwise quality.");
+  description.add_options()
+    ("help", "Print usage information.")
+    ("input-mesh", po::value<std::string>()->required(), "Filename of the input mesh.")
+    ("metric", po::value<int>()->default_value(VTK_QUALITY_RADIUS_RATIO), metric_description.c_str())
+    ("summary", "Print summary.")
+    ("summary-hide-header", "Hide the summary header.")
+    ("cellwise", "Print cellwise data.")
+    ("cellwise-hide-header", "Hide the cellwise header.");
 
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, description), vm);
@@ -68,45 +67,53 @@ main(int argc, char** argv)
 
   const std::string IMesh(vm["input-mesh"].as<std::string>());
   const int metric = vm["metric"].as<int>();
-  const bool cellwise = vm["cellwise"].as<bool>();
+  const bool summary = vm.count("summary");
+  const bool summary_header = !(vm.count("summary-hide-header"));
+  const bool cellwise = vm.count("cellwise");
+  const bool cellwise_header = !(vm.count("cellwise-hide-header"));
 
   const auto reader = TReader::New();
   reader->SetFileName(IMesh.c_str());
+  reader->Update();
 
   const auto quality = TQuality::New();
   quality->SetInputConnection(reader->GetOutputPort());
-  if (cellwise) {
-    quality->SaveCellQualityOn();
-  } else {
-    quality->SaveCellQualityOff();
-  }
+  quality->SetSaveCellQuality(cellwise);
   quality->SetTriangleQualityMeasure(metric);
   quality->Update();
-  const auto fd =
-    quality->GetOutput()->GetFieldData()->GetArray("Mesh Triangle Quality");
-  const auto mn = fd->GetComponent(0, 0);
-  const auto mu = fd->GetComponent(0, 1);
-  const auto mx = fd->GetComponent(0, 2);
-  const auto sd = fd->GetComponent(0, 3);
 
-  std::cout << "Metric: " << quality_map[metric] << std::endl;
-  std::cout << "Min: " << mn << std::endl;
-  std::cout << "Average: " << mu << std::endl;
-  std::cout << "Max: " << mx << std::endl;
-  std::cout << "SD: " << sd << std::endl;
+  if (summary) {
+    const auto fd =
+      quality->GetOutput()->GetFieldData()->GetArray("Mesh Triangle Quality");
+    const auto mn = fd->GetComponent(0, 0);
+    const auto mu = fd->GetComponent(0, 1);
+    const auto mx = fd->GetComponent(0, 2);
+    const auto sd = fd->GetComponent(0, 3);
+
+    if (summary_header) {
+      std::cout << "Metric,Min,Average,Max,SD\n";
+    }
+    std::cout << quality_map[metric] << ','
+              << mn << ','
+              << mu << ','
+              << mx << ','
+              << sd << std::endl;
+  }
 
   if (cellwise) {
     const auto qualityArray = vtkDoubleArray::SafeDownCast(
       quality->GetOutput()->GetCellData()->GetArray("Quality"));
 
-    std::cout << "# Number of cells: " << qualityArray->GetNumberOfTuples()
-              << std::endl;
-    std::cout << "CellID,Area\n";
+    if (cellwise_header) {
+      std::cout << "CellID,Area\n";
+    }
 
     for (vtkIdType i = 0; i < qualityArray->GetNumberOfTuples(); ++i) {
       double val = qualityArray->GetValue(i);
       std::cout << i << ',' << val << '\n';
     }
   }
+
   return EXIT_SUCCESS;
+
 }

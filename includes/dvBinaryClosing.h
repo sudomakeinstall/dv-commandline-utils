6edfe7db96a3ebe4d53bd99886b41f30963f611b
Davis Marc Vigneault <davis.vigneault@gmail.com>
2021-11-14 15:03:32 -0800

    ENH: Add Binary Closing Filter

---
+++ includes/dvBinaryClosing.h
@@ -0,0 +1,44 @@
+#ifndef dv_BinaryClosing_h
+#define dv_BinaryClosing_h
+
+#include <itkBinaryBallStructuringElement.h>
+#include <itkBinaryMorphologicalClosingImageFilter.h>
+#include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
+
+namespace dv {
+
+template<unsigned int Dimension, typename TPixel>
+void
+BinaryClosing(const std::string& IImage,
+              const std::string& OImage,
+              const unsigned int radius)
+{
+  typedef itk::Image<TPixel, Dimension> TImage;
+  typedef itk::ImageFileReader<TImage> TReader;
+  typedef itk::BinaryBallStructuringElement<TPixel, Dimension> TElement;
+  typedef itk::BinaryMorphologicalClosingImageFilter<TImage, TImage, TElement>
+    TOpen;
+  typedef itk::ImageFileWriter<TImage> TWriter;
+
+  const auto reader = TReader::New();
+  reader->SetFileName(IImage);
+
+  TElement structuringElement;
+  structuringElement.SetRadius(radius);
+  structuringElement.CreateStructuringElement();
+
+  const auto closingFilter = TOpen::New();
+  closingFilter->SetInput(reader->GetOutput());
+  closingFilter->SetKernel(structuringElement);
+  closingFilter->SetForegroundValue(1);
+
+  const auto writer = TWriter::New();
+  writer->SetFileName(OImage);
+  writer->SetInput(closingFilter->GetOutput());
+  writer->Update();
+}
+
+}
+
+#endif

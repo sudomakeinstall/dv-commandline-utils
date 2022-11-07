404c2948f033355b187172ae060e5593c981115a
Davis Marc Vigneault <davis.vigneault@gmail.com>
2022-11-06 17:04:29 -0800

    Update Vesselness

--- utils/dv-vesselness.cxx
+++ utils/dv-vesselness.cxx
@@ -4,24 +4,30 @@
 namespace po = boost::program_options;
 
 // ITK
-#include "itkImageFileReader.h"
-#include "itkMultiScaleHessianBasedMeasureImageFilter.h"
-#include "itkHessianToObjectnessMeasureImageFilter.h"
+#include <itkImageFileReader.h>
+#include <itkClampImageFilter.h>
+#include <itkRescaleIntensityImageFilter.h>
+#include <itkGradientAnisotropicDiffusionImageFilter.h>
+#include <itkMultiScaleHessianBasedMeasureImageFilter.h>
+#include <itkHessian3DToVesselnessMeasureImageFilter.h>
 
-#include "itkImageFileWriter.h"
+#include <itkImageFileWriter.h>
 
 // Typedefs
 constexpr unsigned int Dimension = 3;
 
-using PixelType = double;
-using ImageType = itk::Image<PixelType, Dimension>;
-using ReaderType = itk::ImageFileReader<ImageType>;
-using HessianPixelType = itk::SymmetricSecondRankTensor<PixelType, Dimension>;
-using HessianImageType = itk::Image<HessianPixelType, Dimension>;
-using ObjectnessFilterType = itk::HessianToObjectnessMeasureImageFilter<HessianImageType, ImageType>;
-using MultiScaleEnhancementFilterType =
-  itk::MultiScaleHessianBasedMeasureImageFilter<ImageType, HessianImageType, ImageType>;
-using WriterType = itk::ImageFileWriter<ImageType>;
+using TPixel = double;
+using TImage = itk::Image<TPixel, Dimension>;
+using TReader = itk::ImageFileReader<TImage>;
+using TClamp = itk::ClampImageFilter<TImage, TImage>;
+using TRescale = itk::RescaleIntensityImageFilter<TImage>;
+using TDiffusion = itk::GradientAnisotropicDiffusionImageFilter<TImage, TImage>;
+using THessianPixel = itk::SymmetricSecondRankTensor<TPixel, Dimension>;
+using THessianImage = itk::Image<THessianPixel, Dimension>;
+using TVesselnessFilter = itk::Hessian3DToVesselnessMeasureImageFilter<TPixel>;
+using TMultiScaleEnhancement =
+  itk::MultiScaleHessianBasedMeasureImageFilter<TImage, THessianImage, TImage>;
+using TWriter = itk::ImageFileWriter<TImage>;
 
 int
 main(int argc, char** argv)
@@ -33,12 +39,16 @@ main(int argc, char** argv)
     ("help", "Print usage information.")
     ("input-image", po::value<std::string>()->required(), "Filename of input image.")
     ("output-image", po::value<std::string>()->required(), "Filename of output image.")
-    ("alpha", po::value<double>()->default_value(0.5), "Alpha.")
-    ("beta", po::value<double>()->default_value(1.0), "Beta.")
-    ("gamma", po::value<double>()->default_value(5.0), "Gamma.")
-    ("sigma-min", po::value<double>()->default_value(2.0), "Sigma minimum.")
-    ("sigma-max", po::value<double>()->default_value(2.0), "Sigma maximum.")
-    ("sigma-steps", po::value<unsigned int>()->default_value(1), "Sigma steps.");
+    ("clamp-lower", po::value<double>()->default_value(-700.0), "Clamp lower.")
+    ("clamp-upper", po::value<double>()->default_value(1500.0), "Clamp upper.")
+    ("iterations", po::value<unsigned int>()->default_value(10), "Iterations.")
+    ("time-step", po::value<double>()->default_value(0.0625), "Time step.")
+    ("conductance", po::value<double>()->default_value(1.25), "Conductance.")
+    ("alpha1", po::value<double>()->default_value(0.5), "Alpha1.")
+    ("alpha2", po::value<double>()->default_value(2.5), "Alpha2.")
+    ("sigma-min", po::value<double>()->default_value(0.2), "Sigma minimum.")
+    ("sigma-max", po::value<double>()->default_value(3.0), "Sigma maximum.")
+    ("sigma-steps", po::value<unsigned int>()->default_value(8), "Sigma steps.");
 
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, description), vm);
@@ -52,32 +62,48 @@ main(int argc, char** argv)
 
   const std::string IImage = vm["input-image"].as<std::string>();
   const std::string OImage = vm["output-image"].as<std::string>();
-  const double alpha = vm["alpha"].as<double>();
-  const double beta = vm["beta"].as<double>();
-  const double gamma = vm["gamma"].as<double>();
+  const double clamp_lower = vm["clamp-lower"].as<double>();
+  const double clamp_upper = vm["clamp-upper"].as<double>();
+  const unsigned int iterations = vm["iterations"].as<unsigned int>();
+  const double time_step = vm["time-step"].as<double>();
+  const double conductance = vm["conductance"].as<double>();
+  const double alpha1 = vm["alpha1"].as<double>();
+  const double alpha2 = vm["alpha2"].as<double>();
   const double sigma_min = vm["sigma-min"].as<double>();
   const double sigma_max = vm["sigma-max"].as<double>();
   const unsigned int sigma_steps = vm["sigma-steps"].as<unsigned int>();
 
-  const auto reader = ReaderType::New();
+  const auto reader = TReader::New();
   reader->SetFileName(IImage);
 
-  const auto objectnessFilter = ObjectnessFilterType::New();
-  objectnessFilter->SetBrightObject(true);
-  objectnessFilter->SetScaleObjectnessMeasure(false);
-  objectnessFilter->SetAlpha(alpha);
-  objectnessFilter->SetBeta(beta);
-  objectnessFilter->SetGamma(gamma);
+  const auto clamp = TClamp::New();
+  clamp->SetInput( reader->GetOutput() );
+  clamp->SetBounds(clamp_lower, clamp_upper);
+
+  const auto rescale = TRescale::New();
+  rescale->SetInput( clamp->GetOutput() );
+  rescale->SetOutputMaximum( 1.0 );
+  rescale->SetOutputMinimum( 0.0 );
+
+  const auto diffusion = TDiffusion::New();
+  diffusion->SetInput( rescale->GetOutput() );
+  diffusion->SetNumberOfIterations( iterations );
+  diffusion->SetTimeStep( time_step );
+  diffusion->SetConductanceParameter( conductance );
+
+  const auto vesselness = TVesselnessFilter::New();
+  vesselness->SetAlpha1(alpha1);
+  vesselness->SetAlpha2(alpha2);
  
-  const auto multiScaleEnhancementFilter = MultiScaleEnhancementFilterType::New();
+  const auto multiScaleEnhancementFilter = TMultiScaleEnhancement::New();
   multiScaleEnhancementFilter->SetInput( reader->GetOutput() );
-  multiScaleEnhancementFilter->SetHessianToMeasureFilter(objectnessFilter);
+  multiScaleEnhancementFilter->SetHessianToMeasureFilter(vesselness);
   multiScaleEnhancementFilter->SetSigmaStepMethodToLogarithmic();
   multiScaleEnhancementFilter->SetSigmaMinimum(sigma_min);
   multiScaleEnhancementFilter->SetSigmaMaximum(sigma_max);
   multiScaleEnhancementFilter->SetNumberOfSigmaSteps(sigma_steps);
 
-  const auto writer = WriterType::New();
+  const auto writer = TWriter::New();
   writer->SetFileName(OImage);
   writer->SetInput(multiScaleEnhancementFilter->GetOutput());
 
